8c12c41a4844530a1b0b2f08f9c93b488a9046e9
Wanchat <wanchatgame@gmail.com>
2019-10-20 23:34:26 +0700

    1. สร้าง main object TimingsContract
    2.
    สร้าง class AppDatabase
    
    3.
    สร้าง SQL Tasks Table
    
    4.
    สร้าง class SingletonHolder
    
    5.
    onUpgrade และ test
    
    6.
    สร้าง class AppProvider
    
    7.
    uriMatcher
    
    8.
    The Content URI
    
    9.
    Register the ContentProvider
    
    10.
    Using appendWhereEscapeString
    
    11.
    สร้าง main object TasksContract จาก TimingsContract
    
    12.
    เพิ่ม Database ใน insert
    
    13.
    เพิ่ม Update, Delete
    
    14.
    เพิ่ม Insert and Update
    
    15.
    เพิ่ม testDeleteTwo, testUpdateTwo
    
    16.
    Main menu ui
    
    17.
    สร้าง fragment_main, task_list_items และเพิ่มสี
    
    18.
    สร้าง fragment_add_edit
    
    19.
    สร้าง class Task
    
    20.
    สร้าง class AddEditFragment
    
    21.
    Swapping Fragment

--- app/src/main/java/academy/learnprogramming/MainActivity.kt
+++ app/src/main/java/academy/learnprogramming/MainActivity.kt
@@ -1,6 +1,5 @@
 package academy.learnprogramming
 
-import android.content.ContentValues
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,106 +10,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AddEditFragment.OnSaveClicked {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-//        testInsert()
-
-//        testUpdate()
-
-//        testUpdateTwo()
-
-//        testDelete()
-
-        testDeleteTwo()
-
-        val projection = arrayOf(TasksContract.Columns.TASK_NAME, TasksContract.Columns.TASK_SORT_ORDER)
-        val sortColumn = TasksContract.Columns.TASK_SORT_ORDER
-
-//        val cursor = contentResolver.query(TasksContract.buildUriFromId(2),
-        val cursor = contentResolver.query(TasksContract.CONTENT_URI,
-            null,
-            null,
-            null,
-            sortColumn)
-        Log.d(TAG, "*************************")
-        cursor.use {
-            while(it!!.moveToNext()) {
-                // Cycle through all records
-                with(cursor) {
-                    val id = this!!.getLong(0)
-                    val name = getString(1)
-                    val description = getString(2)
-                    val sortOrder = getString(3)
-                    val result = "ID: $id. Name: $name. Description: $description. Sort order: $sortOrder."
-                    Log.d(TAG,"onCreate: reading data $result")
-                }
-            }
-        }
-
-        Log.d(TAG, "*************************")
-
     }
 
-    private fun testDeleteTwo() {
-
-        val selection = TasksContract.Columns.TASK_DESCRIPTION + " = ?"
-        val selectionArgs = arrayOf("For deletion")
-        val rowsAffected = contentResolver.delete(TasksContract.CONTENT_URI,
-            selection,
-            selectionArgs)
-
-        Log.d(TAG, "Number of rows deleted is $rowsAffected")
-    }
-
-    private fun testDelete() {
-
-        val taskUri = TasksContract.buildUriFromId(3)
-        val rowsAffected = contentResolver.delete(taskUri,null, null)
-        Log.d(TAG, "Number of rows deleted is $rowsAffected")
-    }
-
-    private fun testUpdateTwo() {
-        val values = ContentValues().apply {
-            put(TasksContract.Columns.TASK_SORT_ORDER, 999)
-            put(TasksContract.Columns.TASK_DESCRIPTION, "For deletion")
-        }
-
-        val selection = TasksContract.Columns.TASK_SORT_ORDER + " = ?"
-        val selectionArgs = arrayOf("99")
-        val rowsAffected = contentResolver.update(TasksContract.CONTENT_URI,
-            values,
-            selection,
-            selectionArgs)
-
-        Log.d(TAG, "Number of rows updated is $rowsAffected")
-    }
-
-    private fun testUpdate() {
-        val values = ContentValues().apply {
-            put(TasksContract.Columns.TASK_NAME, "Content Provider")
-            put(TasksContract.Columns.TASK_DESCRIPTION, "Record content providers videos")
-        }
-
-        val taskUri = TasksContract.buildUriFromId(4)
-        val rowsAffected = contentResolver.update(taskUri, values, null, null)
-        Log.d(TAG, "Number of rows updated is $rowsAffected")
-    }
-
-    private fun testInsert() {
-        val values = ContentValues().apply {
-            put(TasksContract.Columns.TASK_NAME, "New Task 1")
-            put(TasksContract.Columns.TASK_DESCRIPTION, "Description 1")
-            put(TasksContract.Columns.TASK_SORT_ORDER, 2)
-        }
-
-        val uri = contentResolver.insert(TasksContract.CONTENT_URI, values)
-        Log.d(TAG, "New row id (in uri) is $uri")
-        Log.d(TAG, "id (in uri) is ${TasksContract.getId(uri!!)}")
+    override fun onSaveClicked(){
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -123,9 +33,21 @@ class MainActivity : AppCompatActivity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.menumain_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.menumain_addTask -> taskEditRequest(null)
+//            R.id.menumain_settings -> true
         }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun taskEditRequest(task: Task?) {
+        Log.d(TAG, "taskEditRequest: starts")
+
+        val newFragment = AddEditFragment.newInstance(task)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment, newFragment)
+            .commit()
+
+        Log.d(TAG, "Exiting taskEditRequest")
     }
 }
--- app/src/main/java/academy/learnprogramming/MainActivityFragment.kt
+++ app/src/main/java/academy/learnprogramming/MainActivityFragment.kt
@@ -1,4 +1,15 @@
 package academy.learnprogramming
 
-class MainActivityFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
